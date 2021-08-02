675f2aba446d886fae214b1569b7584f067665fc
Cool <i@nmslwsnd.com>
2021-08-02 18:13:33 +0800

    Delete my-project/target directory

--- my-project/target/rls/debug/deps/my_project-c928b0afed541fc8.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Android\OneDrive - initcool\Rust_learn\my-project\target\rls\debug\deps\my_project-c928b0afed541fc8.rmeta: src\main.rs
-
-c:\Users\Android\OneDrive - initcool\Rust_learn\my-project\target\rls\debug\deps\my_project-c928b0afed541fc8.d: src\main.rs
-
-src\main.rs:
