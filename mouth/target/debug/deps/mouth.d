c8a3aab0f32417876335c3d3efa37e2056984dd6
Cool <i@nmslwsnd.com>
2021-08-02 18:12:19 +0800

    Delete mouth/target directory

--- mouth/target/debug/deps/mouth.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Android\OneDrive - initcool\Rust_learn\mouth\target\debug\deps\mouth.exe: src\main.rs
-
-C:\Users\Android\OneDrive - initcool\Rust_learn\mouth\target\debug\deps\mouth.d: src\main.rs
-
-src\main.rs:
--- mouth/target/debug/mouth.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Android\OneDrive\ -\ initcool\Rust_learn\mouth\target\debug\mouth.exe: C:\Users\Android\OneDrive\ -\ initcool\Rust_learn\mouth\src\main.rs
