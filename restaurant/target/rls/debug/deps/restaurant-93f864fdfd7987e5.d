ee5d9bc73a3cf5aafc3d36fad01863eb561418a6
Cool <i@nmslwsnd.com>
2021-08-02 18:11:58 +0800

    Delete restaurant/target directory

--- restaurant/target/rls/debug/deps/restaurant-93f864fdfd7987e5.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Android\OneDrive - initcool\Rust_learn\restaurant\target\rls\debug\deps\restaurant-93f864fdfd7987e5.rmeta: src\lib.rs
-
-c:\Users\Android\OneDrive - initcool\Rust_learn\restaurant\target\rls\debug\deps\restaurant-93f864fdfd7987e5.d: src\lib.rs
-
-src\lib.rs:
